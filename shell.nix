c7837267e40a0ad9446e03fcda20340ae7006360
Martin Lavoie <broemartino@gmail.com>
2020-11-13 21:50:10 +0100

    Setup with fast compile dependency from nix

--- shell.nix
+++ shell.nix
@@ -1,15 +1,13 @@
 { 
 sources ? import ./nix/sources.nix, 
-mozilla-overlay ? import sources.nixpkgs-mozilla,
 pkgs ? import sources.nixpkgs { config.allowUnfree = true; overlays = [ mozilla-overlay ]; },
+mozilla-overlay ? import sources.nixpkgs-mozilla,
 nixGL ? import sources.nixGL { inherit pkgs; }
 }:
 let 
   nightly-rust = pkgs.latest.rustChannels.nightly;
-  rust = nightly-rust.rustc;
-  cargo = pkgs.cargo;
+  rust = nightly-rust.rust;
 
-  steam-run = pkgs.steam-run;
   wrap = name: pkg: pkgs.stdenv.mkDerivation {
     name = "steam-run-${name}";
     version = "dontcare";
@@ -18,14 +16,16 @@ let
     nativeBuildInputs = [ pkgs.makeWrapper ];
     buildCommand = ''
       mkdir -p $out/bin
-      makeWrapper ${steam-run}/bin/steam-run $out/bin/${name} --add-flags ${pkg}/bin/${name}
+      makeWrapper ${pkgs.steam-run}/bin/steam-run $out/bin/${name} --add-flags ${pkg}/bin/${name}
     '';
   };
-  wrapCargo = wrap "cargo" cargo;
+
+  wrapCargo = wrap "cargo" rust;
   wrapCoz = wrap "coz" pkgs.coz;
 in pkgs.mkShell {
   buildInputs = [
     wrapCargo
+    rust
     pkgs.alsaLib
     pkgs.pkgconfig
     pkgs.libudev
@@ -36,8 +36,8 @@ in pkgs.mkShell {
     pkgs.xorg.libXcursor
     pkgs.xorg.libXi
     pkgs.xorg.libXrandr
-    #nixGL.nixVulkanNvidia
     pkgs.lld
+    pkgs.clang
     pkgs.cargo-watch
     pkgs.aseprite
     wrapCoz
